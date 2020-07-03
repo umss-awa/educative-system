06e026a793ec5c335c8f57a6ccf184f9f7a287da
Carlos Torrico Lupe <carlosalbertotorricolupe@gmail.com>
2020-07-02 22:29:34 -0400

    Agregando express, mongoose,coors y la conexion a la BD

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import path from 'path';
+import mongoose from 'mongoose';
+
+//Conexion BD2
+const uri = 'mongodb://localhost:27017/BDproyecto';
+const options = {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true};
+mongoose.connect(uri, options).then(() => { 
+    console.log('Conectado a la BD en el puerto 27017')},
+    err => {err }
+  );
+
+const app=express();
+const { urlencoded } = require('express');
+app.use(morgan('dev'));
+app.use(cors());
+
+app.use(express.json());
+app.use(urlencoded({extended:true}));
+app.use(express.static(path.join( __dirname, 'public')));
+
+app.set('port',process.env.PORT || 3000);
+ 
+app.listen(app.get('port'),()=>{
+    console.log(`service on in the port ${app.get('port')}`);
+}); 
